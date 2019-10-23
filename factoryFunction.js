e95001651af0ad358851b38b9b0f843e2686c7f7
Mason Reichbauer <mreichba@mail.usf.edu>
2019-10-23 17:31:10 -0400

    factory function continued

---
+++ factoryFunction.js
@@ -0,0 +1,16 @@
+'use strict';
+function creatCharacter(){
+name: ,
+nickname: ,
+race: ,
+origin: ,
+attack: ,
+defense: ,
+describe: function(){
+  console.log(`${this.name} is a ${this.race} from ${this.origin}.`);
+}
+evaluateFight: function(character){
+  console.log(`Your opponent takes ${x} damage and you receive ${y} damage`);
+}
+
+}
